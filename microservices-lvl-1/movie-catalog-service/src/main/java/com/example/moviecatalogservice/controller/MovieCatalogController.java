573dcf4902f97d0f498371df670b1b9698164bde
elivelton gustavo da silva <elivelton.gustavo34@gmail.com>
2021-02-03 02:00:32 -0300

    Converted services to Eureka clients and finished project 1

--- microservices-lvl-1/movie-catalog-service/src/main/java/com/example/moviecatalogservice/controller/MovieCatalogController.java
+++ microservices-lvl-1/movie-catalog-service/src/main/java/com/example/moviecatalogservice/controller/MovieCatalogController.java
@@ -4,6 +4,7 @@ import com.example.moviecatalogservice.entity.CatalogItem;
 import com.example.moviecatalogservice.entity.Movie;
 import com.example.moviecatalogservice.entity.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +22,17 @@ public class MovieCatalogController {
     private RestTemplate restTemplate;
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service:8083/ratingsdata/users/" + userId, UserRating.class);
         return ratings.getUserRating().stream().map(rating -> {
             // For each movie ID, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service:8082/movies/" + rating.getMovieId(), Movie.class);
             // Put them all together
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         })
